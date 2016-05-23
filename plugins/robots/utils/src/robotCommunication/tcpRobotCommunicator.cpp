40ff618d1c7a5ffde59db71000431a8ae4b75e7f
greg <zimin_grigory@hotmail.com>
2016-05-23 21:10:39 +0300

    master upd: can receive message from robot
    Signed-off-by: greg <zimin_grigory@hotmail.com>

--- plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp
+++ plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp
@@ -155,6 +155,9 @@ void TcpRobotCommunicator::onMessageFromRobot(const MessageKind &messageKind, co
 	case MessageKind::fileContents:
 		emit fileContentsFromRobot(message);
 		break;
+	case MessageKind::mail:
+		emit mailFromRobot(message);
+		break;
 	}
 }
 
